7b6cee75f46d55a947d1ddcf63658573255414c8
Andrew Yates <ayates@ebi.ac.uk>
2018-01-31 07:14:04 +0000

    Configure hypnotoad

--- lib/Fastadb/App.pm
+++ lib/Fastadb/App.pm
@@ -14,10 +14,24 @@ has schema => sub {
 
 sub startup {
   my ($self) = @_;
+
+  # Configure hypnotoad
+  if(exists $ENV{APP_PID_FILE}) {
+    $self->config(hypnotoad => {
+      proxy => 1,
+      pid_file => $ENV{APP_PID_FILE},
+    });
+  }
+
+  # Install the schema helper
   $self->helper(db => sub { $self->app()->schema() });
 
   # Route commands through the application
   my $r = $self->routes;
+  $r->get('/ping' => {ping => ''} => sub {
+    my ($c) = @_;
+    $c->render(text => "Ping");
+  });
   $r->get('/sequence/:id')->to(controller => 'seq', action => 'id');
   $r->get('/metadata/:id')->to(controller => 'metadata', action => 'id');
 
