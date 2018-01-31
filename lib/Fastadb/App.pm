20ceea74a9a80d4b6f5412596adb5610975fa07c
Andrew Yates <ayates@ebi.ac.uk>
2018-01-31 15:42:24 +0000

    Support CORS

--- lib/Fastadb/App.pm
+++ lib/Fastadb/App.pm
@@ -23,21 +23,47 @@ sub startup {
     });
   }
 
+  $self->cors();
+
   # Install the schema helper
   $self->helper(db => sub { $self->app()->schema() });
 
   # Route commands through the application
   my $r = $self->routes;
+
+  #Routes that just work inline
   $r->get('/ping' => {ping => ''} => sub {
-    my ($c) = @_;
+    my $c = shift;
     $c->render(text => "Ping");
   });
+  $r->options('/options' => sub {
+    my $c = shift;
+    $c->render(text => "OPTIONS");
+  });
+
+  # Things that go to a controller
   $r->get('/sequence/:id')->to(controller => 'seq', action => 'id');
   $r->get('/metadata/:id')->to(controller => 'metadata', action => 'id');
   $r->post('/batch/sequence')->to(controller => 'batchseq', action => 'batch');
 
   # New content type of FASTA
   $self->types->type(fasta => 'text/x-fasta');
+}
+
+sub cors {
+  my ($self) = @_;
+  $self->hook(
+    before_dispatch => sub {
+      my $c = shift;
+      if($c->req->headers->header('Origin')) {
+        $c->res->headers->header( 'Access-Control-Allow-Origin' => '*' );
+        $c->res->headers->header( 'Access-Control-Allow-Methods' => 'GET, OPTIONS' );
+        $c->res->headers->header( 'Access-Control-Max-Age' => 2592000 );
+        $c->res->headers->header( 'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With' );
+      }
+    }
+  );
+  return;
 }
 
 1;
