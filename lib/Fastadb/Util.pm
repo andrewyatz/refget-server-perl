cf8adc296a7a5c45836b496eac8d601704123799
Andrew Yates <andrewyatz@users.noreply.github.com>
2018-04-16 17:53:58 +0100

    Change subroutine name and implement exporter

--- lib/Fastadb/Util.pm
+++ lib/Fastadb/Util.pm
@@ -3,9 +3,13 @@ package Fastadb::Util;
 use strict;
 use warnings;
 
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(reference_retrieval_digest);
+
 use Digest::SHA qw/sha512_hex/;
 
-sub reference_retrieval_checksum {
+sub reference_retrieval_digest {
   my ($seq, $offset) = @_; # offset expressed in bytes
   $offset = 24 if ! defined $offset;
   my $digest = sha512_hex($sequence);
