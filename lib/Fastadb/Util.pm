17a0b717eaf16b552643c27a78bcb6c6d808ea9e
Andrew Yates <andrewyatz@users.noreply.github.com>
2018-04-16 17:59:59 +0100

    Bad reference

--- lib/Fastadb/Util.pm
+++ lib/Fastadb/Util.pm
@@ -10,7 +10,7 @@ our @EXPORT_OK = qw(reference_retrieval_digest);
 use Digest::SHA qw/sha512_hex/;
 
 sub reference_retrieval_digest {
-  my ($seq, $offset) = @_; # offset expressed in bytes
+  my ($sequence, $offset) = @_; # offset expressed in bytes
   $offset = 24 if ! defined $offset;
   my $digest = sha512_hex($sequence);
   my $substring = substr($digest, 0, $offset*2); #going into hex from bytes
