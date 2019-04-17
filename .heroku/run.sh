845b49d97bab733e5ffc2de059d2364a9afdbaac
Andrew Yates <andyyatz@gmail.com>
2019-04-17 19:26:13 +0100

    Trying to get heroku to build this config correctly

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -4,4 +4,5 @@
 tar zxf compliance-data.tar.gz
 
 # Substiute the path of the build dir into the heroku config
+cp $BUILD_DIR/.heroku/refget-app.heroku.json.template $BUILD_DIR/.heroku/refget-app.heroku.json
 perl -pi -e 's/%([^%]+)%/$ENV{$1}/g' $BUILD_DIR/.heroku/refget-app.heroku.json
