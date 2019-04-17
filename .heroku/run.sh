ea81888ae621c69537a092fd31c3ef3fa3a0e7ad
Andrew Yates <andyyatz@gmail.com>
2019-04-17 19:19:25 +0100

    No need for . just decompress the tarball

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Execute in $BUILD_DIR (which should be here)
-tar zxf compliance-data.tar.gz .
+tar zxf compliance-data.tar.gz
 
 # Substiute the path of the build dir into the heroku config
 perl -pi -e 's/%([^%]+)%/$ENV{$1}/g' $BUILD_DIR/.heroku/refget-app.heroku.json
